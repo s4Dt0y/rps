725d027c068c460c2a2e2fc37672b81241b8a023
s4:D_t0y <roccowu949@gmail.com>
2024-02-27 17:35:22 -0800

    Create choice enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
+#[derive(Debug)]
+enum Choice {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+impl Choice {
+    fn parse(input: String) -> Result<Choice, String> {
+        if input == "r".to_string() {
+            return Ok(Choice::ROCK);
+        } else if input == "p".to_string() {
+            return Ok(Choice::PAPER);
+        } else if input == "s".to_string() {
+            return Ok(Choice::SCISSORS);
+        } else {
+            return Err(format!("{:?} Invalid input", input));
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, Rock-Paper-Scissors!");
+    let mut input: String = String::new();
+
+    println!("[r,p,s]:");
+    io::stdin().read_line(&mut input).expect("sorry");
+
+    let x = Choice::parse(input.trim().to_string()).expect("none");
+    println!("{:?}", x);
 }
